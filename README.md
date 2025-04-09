ed65836f55358dbea0c0634712ad10d755dd4b55
Luis Martinez <luis.7martinez.10@gmail.com>
2025-04-09 07:58:21 -0400

    add project

---
+++ README.md
@@ -0,0 +1,110 @@
+# Blender animations using human pose estimation models
+
+This repo contains all files and samples necessaries to estimate pose, save the points of each frame of a video and upload those frames into Blender.
+
+Test were made using two repositories.
+
+https://github.com/jiajunhua/ildoonet-tf-pose-estimation  
+https://github.com/DenisTome/Lifting-from-the-Deep-release
+
+## Install
+
+### Requirements
+- CUDA (Nvidia GPU) 10.0 version: (tested with NVIDIA GEFORCE GTX 1650 Ti 4 GB)
+- NVIDIA graphics card with at least 1.2 GB available.
+- Highly recommended: cuDNN 7.6.5 or compatible version for CUDA.
+
+Change these lines in estimator.py from the first repository and \_pose_estimator.py from the second for your specific needs.
+```
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+```
+
+I set the limit up to 30% of my GPU memory.
+
+### Clone and install requirements
+
+1. Create conda environment with Python 3.6
+
+2. Clone the repo and install 3rd-party libraries from requirements.txt.
+
+```bash
+$ git clone https://github.c/OscarSantosMu/TF_pose_estimation_and_Blender
+$ cd TF_pose_estimation_and_Blender
+$ pip install -r requirements.txt
+```
+
+*Dependencies*
+
+You need dependencies below.
+
+- tensorflow-gpu 1.16
+- opencv3, protobuf, python3-tk
+- slidingwindow
+  - https://github.com/adamrehn/slidingwindow
+  - With some changes from the above git repo.
+
+
+Build c++ library for post processing. See : https://github.com/ildoonet/tf-pose-estimation/tree/master/tf_pose/pafprocess
+```
+$ cd tf_pose/pafprocess
+$ swig -python -c++ pafprocess.i && python3 setup.py build_ext --inplace
+```
+
+### Blender
+
+- Blender 2.91
+
+Probably it works in different versions as well, but it was tested using that version.
+
+## Repositories
+
+To test first repo follow the following [instructions](https://github.com/OscarSantosMu/ildoonet-tf-pose-estimation)
+
+To test second repo go to the following [folder](https://github.com/OscarSantosMu/TF_pose_estimation_and_Blender/LiftingfromtheDeeprelease)
+
+## Demo
+
+1. Run [save\_points.py](https://github.com/OscarSantosMu/TF_pose_estimation_and_Blender/LiftingfromtheDeeprelease/applications/save_points.py)
+
+You will see that json file is generated with 17 points estimated by Lifting from the Deep model. The idea is to save 17 points of each frame of a video. 
+
+![](static/p1_2d.png)
+
+![](static/p1_3d.png)
+
+Therefore, same code was implemented in [camera.py](https://github.com/OscarSantosMu/TF_pose_estimation_and_Blender/LiftingfromtheDeeprelease/applications/camera.py)
+
+2. Run [use\_points.py](https://github.com/OscarSantosMu/TF_pose_estimation_and_Blender/LiftingfromtheDeeprelease/applications/use_points.py)
+
+This will access to the 17 points saved from the previous script and print them in the terminal.
+
+3. Run [camera.py](https://github.com/OscarSantosMu/TF_pose_estimation_and_Blender/LiftingfromtheDeeprelease/applications/camera.py)
+
+This generates the json file that will be imported in Blender.
+
+4. Open [BodyMaleTemplate_spheres.blend](https://github.com/OscarSantosMu/TF_pose_estimation_and_Blender/LiftingfromtheDeeprelease/applications/BodyMaleTemplate_spheres.blend), then run spheres_test.py while selecting all spheres from the canvas.
+
+This script creates the animation.
+
+5. Clic play on the timeline.
+
+![](static/frames.JPG)
+
+Make sure to modify end value if your recording is too long.
+
+![](static/original_video.gif)
+
+![](static/leg.gif)
+
+Enjoy it!
+
+Here are some comparisons between video, matplotlib 3d figure and Blender.
+
+![](static/estimation1.JPG)
+
+![](static/estimation2.JPG)
+
+![](static/estimation3.JPG)
+
+![](static/estimation4.JPG)
---
+++ run_3d.py
@@ -0,0 +1,134 @@
+import argparse
+import logging
+import sys
+import time
+
+from tf_pose import common
+import cv2
+import numpy as np
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import get_graph_path, model_wh
+from lifting.utils.prob_model import Prob3dPose
+from lifting.utils import plot_pose
+
+logger = logging.getLogger('TfPoseEstimatorRun')
+logger.handlers.clear()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tf-pose-estimation run')
+    parser.add_argument('--image', type=str, default='./images/p1.jpg')
+    parser.add_argument('--model', type=str, default='cmu',
+                        help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    parser.add_argument('--resize', type=str, default='0x0',
+                        help='if provided, resize images before they are processed. '
+                             'default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
+
+    args = parser.parse_args()
+
+    w, h = model_wh(args.resize)
+    if w == 0 or h == 0:
+        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
+    else:
+        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+
+    # estimate human poses from a single image !
+    image = common.read_imgfile(args.image, None, None)
+    if image is None:
+        logger.error('Image can not be read, path=%s' % args.image)
+        sys.exit(-1)
+
+    t = time.time()
+    humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+    elapsed = time.time() - t
+
+    logger.info('inference image: %s in %.4f seconds.' % (args.image, elapsed))
+
+    image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+
+    try:
+        import matplotlib.pyplot as plt
+
+        fig = plt.figure()
+        a = fig.add_subplot(2, 2, 1)
+        a.set_title('Result')
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+        bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
+        bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
+
+        # show network output
+        a = fig.add_subplot(2, 2, 2)
+        plt.imshow(bgimg, alpha=0.5)
+        tmp = np.amax(e.heatMat[:, :, :-1], axis=2)
+        plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
+        plt.colorbar()
+
+        tmp2 = e.pafMat.transpose((2, 0, 1))
+        tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
+        tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
+
+        a = fig.add_subplot(2, 2, 3)
+        a.set_title('Vectormap-x')
+        # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
+        plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
+        plt.colorbar()
+
+        a = fig.add_subplot(2, 2, 4)
+        a.set_title('Vectormap-y')
+        # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
+        plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
+        plt.colorbar()
+        plt.show()
+    except Exception as e:
+        logger.warning('matplitlib error, %s' % e)
+        cv2.imshow('result', image)
+        cv2.waitKey()
+
+    logger.info('3d lifting initialization')
+    poseLifting = Prob3dPose('LiftingfromtheDeeprelease/data/saved_sessions/prob_model/prob_model_params.mat')
+
+    image_h, image_w = image.shape[:2]
+    standard_w = 640
+    standard_h = 480
+
+    pose_2d_mpiis = []
+    visibilities = []
+    for human in humans:
+        pose_2d_mpii, visibility = common.MPIIPart.from_coco(human)
+        pose_2d_mpiis.append([(int(x*standard_w + 0.5), int(y*standard_h + 0.5)) for x, y in pose_2d_mpii])
+        visibilities.append(visibility)
+
+    pose_2d_mpiis = np.array(pose_2d_mpiis)
+    visibilities = np.array(visibilities)
+    transformed_pose2d, weights = poseLifting.transform_joints(pose_2d_mpiis, visibilities)
+    print(pose_2d_mpiis)
+    print(transformed_pose2d)
+    pose_3d = poseLifting.compute_3d(transformed_pose2d, weights)
+    np.set_printoptions(formatter={'float_kind':'{:f}'.format})
+    print(type(pose_3d))
+
+    for i, single_3d in enumerate(pose_3d):
+        print(single_3d)
+        plot_pose(single_3d)
+
+    plt.show()
+
+    """ pose_3dxyz = np.array(pose_3d[0]).transpose()
+    for point in pose_3dxyz:
+        print(point)
+    
+    # Show 3D poses
+    for single_3D in pose_3d:
+        # or plot_pose(Prob3dPose.centre_all(single_3D))
+        plot_pose(single_3D)
+
+    plt.show() """
